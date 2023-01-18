d3e1e9a5bceb3f9fc4e3e9bbc2e93cac9c71bc99
Cameron Miner <cameronsminer@gmail.com>
2023-01-18 10:52:08 -0700

    Removed procfile and added new scripts and server code to deploy

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { db } = require("./util/database");
 const { PORT } = process.env || 5555; //5555
 const seed = require("./util/seed");
 const express = require("express");
+const path = require('path')
 const cors = require("cors"); // Client and Server can run on different ports and still share resources across 'origins'
 const { CartItem } = require("./models/cartItem");
 const { Photo } = require("./models/photo");
@@ -30,6 +31,9 @@ const {
 server.use(express.json()); // Parse requests into JSON
 server.use(cors()); // Client & Server can run on seperate ports
 
+// For Heroku deployment
+server.use(express.static(path.resolve(__dirname, '../build')));
+
 //! Associations (relations). Join tables. DB will use this to create connections in CartItem table and Photo table.
 // Users and Products
 User.hasMany(CartItem);
@@ -49,6 +53,11 @@ server.get("/cart/:userId", getUserCart);
 server.post("/cartitem/:id", addToCart);
 server.delete('/cartitem/:cartItemId', deleteFromCart)
 server.delete('/cart/:userId', deleteCart)
+
+// Catch-all endpoint for Heroku deployment
+server.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 // Listening
 // Sync models/associations to DB and have server listen. This will sync DB before server starts up
